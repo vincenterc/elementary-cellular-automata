e5638505bdb6632adbd9a6a3bb664f46c41c3849
vincenterr <chung.menghsun@gmail.com>
2019-05-16 18:13:33 +0800

    Modify home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,16 @@
 import React from 'react'
 import PageWrapper from '../components/page-wrapper'
+import P5Wrapper from '../components/p5-wrapper'
+import elementaryCASketch from '../sketches/elementary-cellular-automata'
 
-export default PageWrapper(() => <div>Gatsby starter!</div>)
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <P5Wrapper sketch={elementaryCASketch} />
+      </div>
+    )
+  }
+}
+
+export default PageWrapper(HomePage)
