5b030a857859e829c993941e388d574586f48f6b
vincenterr <chung.menghsun@gmail.com>
2019-05-21 16:40:47 +0800

    Modify gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  pathPrefix: '/elementary-cellular-automata',
+  pathPrefix: '/elementary-cellular-automaton',
   plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`],
 }
