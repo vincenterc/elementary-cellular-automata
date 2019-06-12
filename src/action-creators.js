6a6a0f14d22b476b3496db412f957ba2759e6f0f
vincenterr <chung.menghsun@gmail.com>
2019-06-12 08:14:36 +0800

    Remove unused code

--- src/action-creators.js
+++ src/action-creators.js
@@ -2,10 +2,6 @@ import * as actionTypes from './reducers/action-types'
 
 export default function(dispatch) {
   return {
-    setElementaryCARule: rule => {
-      dispatch({ type: actionTypes.SET_ELEMENTARY_CA_RULE, payload: rule })
-    },
-
     setElementaryCAState: state => {
       dispatch({
         type: actionTypes.SET_ELEMENTARY_CA_STATE,
